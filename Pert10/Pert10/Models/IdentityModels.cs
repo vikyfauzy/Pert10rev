9562c313cb61ce1dc6fad84b6aab6ee56f02a480
viky fauzy <viky.fauzy@gmail.com>
2017-11-07 20:18:50 +0700

    menambah spasi menambah enter pada home menambah enter pada values mengubah spasi pada AccountBindingModels

--- Pert10/Pert10/Models/IdentityModels.cs
+++ Pert10/Pert10/Models/IdentityModels.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
 
 namespace Pert10.Models
-{
+{()
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
